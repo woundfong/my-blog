c39175ccc1075f2bbcb53108b34ca64b038308fd
woundfong <woundfongv2@163.com>
2017-09-02 10:55:54 +0800

    welcome

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist')));
+app.use(express.static(path.join(__dirname, 'front/dist')));
 
 app.use('/', index);
 app.use('/users', users);
