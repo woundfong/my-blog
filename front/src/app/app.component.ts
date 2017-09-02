e55561ee6301a881b033e62ca940918ee8d9d4a0
woundfong <woundfongv2@163.com>
2017-09-02 19:54:48 +0800

    test 修改初始值

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   constructor(){}
-  private isCollapsed:boolean = true;
+  private isCollapsed:boolean = false;
   ngOnInit() {
   }
   collapseChange(){
