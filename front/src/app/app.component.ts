cf20d46cec0bef0a23ef24e8337c9b4c35b31440
woundfong <woundfongv2@163.com>
2017-09-17 21:06:14 +0800

    响应式布局

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   constructor(){}
   private isCollapsed:boolean = false;
+  private isMediaShow:boolean = false;
+  private isScrollTop:boolean = false;
+  // @ViewChild('boxShadow') private boxShadow: ElementRef;
   ngOnInit() {
+    let _this = this;
+    window.onscroll = function() {
+      let top = document.documentElement.scrollTop || document.body.scrollTop;
+      if(top >= 57){
+        _this.isScrollTop = true;
+      }else{
+        _this.isScrollTop = false;
+      }
+      
+    }
   }
-  collapseChange(){
-    console.log(this.isCollapsed);
+  changeCollapsed() {
+    let width = document.body.offsetWidth;
+    if(width < 1000){
+      this.isMediaShow = !this.isMediaShow;
+    }else{
+      this.isCollapsed = !this.isCollapsed;
+    }
+  }
+  hideShadow() {
+    this.isMediaShow = !this.isMediaShow;
   }
 }
